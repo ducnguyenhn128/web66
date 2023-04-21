8b34aa183c4338b93d8404813a8e398f5189e47c
ducnguyenhn128 <ducnguyen.dautunhanha@gmail.com>
2023-04-21 23:25:44 +0700

    post page

--- b1/page/src/components/profile/profile.js
+++ b1/page/src/components/profile/profile.js
@@ -1,38 +1,69 @@
 import React from 'react';
-import {Container, Row, Col, Nav} from 'reactstrap'
+import {Container, Row, Col, Nav, NavLink} from 'reactstrap'
 import './styles.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
+import trump from './../../img/trump.jpg'
 const Profile = () => {
     return (  
-        // <Container>
-        //     <Row>
-        //         <Col xs={3}>
-        //             <div>
-        //                 <img></img>
-        //                 <h3>Donald Duck </h3>
-        //                 <p>Hanoi Vietnam</p>                 
-        //             </div>
+        <Container>
+            <Row>
+                <Col xs={3}>
+                    <div className='d-flex flex-column align-items-center justify-content-center'>
+                        <img src={trump} alt='donald'></img>
+                        <h3 className="text-center">Donald Duck </h3>
+                        <p className="text-center">Hanoi Vietnam</p>                 
+                    </div>
 
-        //             <div className='profile-more'>
-        //                 <Nav>Profile</Nav>
-        //                 <Nav>Posts</Nav>
-        //                 <Nav>Follows</Nav>
+                    <div className='profile-more'>
+                        <Nav className='mb-1'>Profile</Nav>
+                        <Nav className='mb-1'>Posts</Nav>
+                        <Nav className='mb-1'>Follows</Nav>
+                        
+                        <hr />
+                        <Nav className='mb-1'>My Info</Nav>
+                        <Nav className='mb-1'>Privacy</Nav>
+                        <Nav className='mb-1'>Password</Nav>
 
-        //                 <Nav>My Info</Nav>
-        //                 <Nav>Privacy</Nav>
-        //                 <Nav>Password</Nav>
+                    </div>
+                </Col>  
 
-        //             </div>
-        //         </Col>  
+                <Col xs={9}>
+                    <Row>
 
-        //         <Col xs={9}>
-        //             <p>Post</p>
-        //         </Col>
-        //     </Row>
-        // </Container>
-        <p className='fs-1'>Test</p>
+                    </Row>
+                    <Row className='my-4'>
+                        <Col className='px-0'><h3 >Recent Posts</h3></Col>
+                        <Col>
+                            <NavLink style={{ textAlign: 'right', marginTop: '10px' }}>View All &gt;&gt;</NavLink>
+                        </Col>
+                    </Row>
+                    {/* Posts here */}
+                    <Row>
+                        <div className ='post'>
+                            <p>The woods are lovely, dark and deep, <br />
+                                But I have promises to keep, <br />
+                                And miles to go before I sleep, <br />
+                                And miles to go before I sleep. <br />
+                            </p>
+                            <p>20 likes   5 comments</p>
+                            <p>#hashtag1 #hashtag2</p>
+                        </div>
+
+                        <div className ='post'>
+                            <p>Hope is the thing with feathers <br />
+                                That perches in the soul, <br />
+                                And sings the tune without the words, <br />
+                                And never stops at all <br />
+                            </p>
+                            <p>20 likes   5 comments</p>
+                            <p>#hashtag1 #hashtag2</p>
+                        </div>
+                    </Row>
+                </Col>
+            </Row>
+        </Container>
+        // <p className='fs-1'>Test</p>
     );
 }
  
