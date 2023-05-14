a516b15314e7dbdabb2f3ca8dc7fe195cdafadeb
ducnguyenhn128 <ducnguyen.dautunhanha@gmail.com>
2023-05-14 23:22:55 +0700

    done

---
+++ attempt2/app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const router = require('./src/controllers/router')
+const PORT = process.env.PORT || 8000;
+const bodyParser = require('body-parser');
+const app = express();
+const cors = require('cors');
+
+
+// For handlebars
+const path = require('path');
+const {engine} = require('express-handlebars')
+
+// CORS
+app.use(cors({
+    origin: 'http://localhost:3000'
+  }));
+  
+// Body parser
+app.use(bodyParser.json())
+
+// HandleBars Template Engine
+app.engine('hbs', engine({
+    extname: '.hbs'
+}))
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, '/src/views'))
+
+// console.log('PATH: ', path.join(__dirname), 'views')
+
+// API End Point
+app.get('/message', (req, res) => {
+    res.json({ message: "Hello from server!" });
+})
+// app.get('/', (req, res) => {
+//     res.render('home')
+// })
+
+// app.get('/news', (req, res) => {
+//     res.render('news')
+// })
+
+app.use('/api', router)
+
+app.listen(PORT, () => {
+    console.log(`Server is listening at ${PORT}`);
+})
---
+++ attempt2/readme.md
@@ -0,0 +1,6 @@
+Connect Node JS with ExpressJS
+
+1. Dev NodeJS normal. Remember to use CORS
+2. In the Views folder , run npx create-react-app .
+This will create a FE react JS inside
+3. Fetch APi in React to receive Data from backend Node JS
