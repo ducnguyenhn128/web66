d340d850ffb39c40dea1944a3e4f582efece80f9
ducnguyenhn128 <ducnguyen.dautunhanha@gmail.com>
2023-05-18 17:27:05 +0700

    search in nav bar

--- attempt2/src/views/src/header/index.jsx
+++ attempt2/src/views/src/header/index.jsx
@@ -5,22 +5,30 @@ import MenuIcon from '@mui/icons-material/Menu';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import MessageIcon from '@mui/icons-material/Message';
 import PersonIcon from '@mui/icons-material/Person';
+import { TextField } from '@mui/material';
+// import { makeStyles } from '@mui/styles';
+
+
 
 const Header = () => {
+
+
   return (
     <div class='home-header'>
       <div class='d-flex justify-content-between bg-success p-2'>
-          <div class='col-3 my-auto text-white'>
+          <div class='col-2 my-auto text-white'>
               {/* <img src={favicon} alt='logo' style={{width: '100px'}} /> */}
               Open Space
           </div>
 
           <div class='col-2 my-auto text-white'>
-              <input placeholder='Search here'></input>
-              
+              <input 
+                placeholder='Search ...'
+                className='header-search-input'
+              />
           </div>
 
-          <div class='col-4 my-auto text-white'>
+          <div class='my-auto text-white flex-grow-1 text-end'>
               <a href='/' class='me-4 text-white fs-3' ><MenuIcon /></a>
               <a href='/' class='me-4 text-white fs-3'><NotificationsIcon /></a>
               <a href='/' class='me-4 text-white fs-3'><MessageIcon /></a>
